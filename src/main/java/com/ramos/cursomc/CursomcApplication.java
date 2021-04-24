c386107c25cd00f5ae98aab13d25e7f6530c0230
Wilian Ramos <wilianramos.2014@gmail.com>
2021-04-24 14:50:44 -0300

    Criando iperacao de instanciacao

--- src/main/java/com/ramos/cursomc/CursomcApplication.java
+++ src/main/java/com/ramos/cursomc/CursomcApplication.java
@@ -1,13 +1,33 @@
 package com.ramos.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.ramos.cursomc.domain.Categoria;
+import com.ramos.cursomc.repositories.CategoriaRepository;
+
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRespository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRespository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
